f1c59f02bd926531e19dfafdcbe426765652c80d
Michael Albaladejo <m.albaladejo@lombardodier.com>
2018-02-20 14:04:36 +0100

    Icon font in menu

--- Mal.XF.Wallpaper/Mal.XF.Wallpaper/Pages/Configuration/ConfigurationDisplayableToken.cs
+++ Mal.XF.Wallpaper/Mal.XF.Wallpaper/Pages/Configuration/ConfigurationDisplayableToken.cs
@@ -1,4 +1,5 @@
-﻿using Mal.XF.Infra.Navigation;
+﻿using Mal.XF.Infra.Fonts;
+using Mal.XF.Infra.Navigation;
 using Mal.XF.Wallpaper.Localisation;
 
 namespace Mal.XF.Wallpaper.Pages.Configuration
@@ -6,7 +7,7 @@ namespace Mal.XF.Wallpaper.Pages.Configuration
     internal class ConfigurationDisplayableToken : IDisplayableNavigationToken
     {
         public INavigationToken NavigationToken { get; } = new ConfigurationToken();
-        public string Icon => "#";
+        public string Icon => IconFont.Cog;
         public string Label => TranslationKeys.Settings;
         public int DisplayOrder => 2;
     }
--- Mal.XF.Wallpaper/Mal.XF.Wallpaper/Pages/Main/MainDisplayableToken.cs
+++ Mal.XF.Wallpaper/Mal.XF.Wallpaper/Pages/Main/MainDisplayableToken.cs
@@ -1,4 +1,5 @@
-﻿using Mal.XF.Infra.Navigation;
+﻿using Mal.XF.Infra.Fonts;
+using Mal.XF.Infra.Navigation;
 using Mal.XF.Wallpaper.Localisation;
 
 namespace Mal.XF.Wallpaper.Pages.Main
@@ -6,7 +7,7 @@ namespace Mal.XF.Wallpaper.Pages.Main
     internal class MainDisplayableToken : IDisplayableNavigationToken
     {
         public INavigationToken NavigationToken { get; } = new MainToken();
-        public string Icon => "#";
+        public string Icon => IconFont.Home;
         public string Label => TranslationKeys.Home;
         public int DisplayOrder => 1;
     }
